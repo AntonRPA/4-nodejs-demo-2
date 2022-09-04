1dc469c6d6356bf1370e3a5f1e9c892766d59c01
AntonRPA <jobtakeranton@mail.ru>
2022-09-05 00:30:44 +0300

    8.5 Работа с файловой системой

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,16 +1,36 @@
 import { homedir } from 'os';
-import { join, basename, dirname, extname, relative, isAbsolute, resolve, sep } from 'path';
+import { join } from 'path';
+import { promises } from 'fs';
 
-const filePath = join(homedir(), 'Node_Lesson/weather_app/weather-data.json');
+const filePath = join(homedir(), 'weather-data.json');
 
-const saveKeyValue = (key, value) => {
-	console.log(basename(filePath)); //Имя конечного файла или папки в пути
-	console.log(dirname(filePath)); //Имя дерриктории конечной
-	console.log(extname(filePath)); //Расширения файла из пути
-	console.log(relative(filePath, dirname(filePath))); //Относительный путь между двумя путями которые мы передали. "Из" 1 пути "Во" 2 путь
-	console.log(isAbsolute(filePath)); //Проверяет путь относительный (с точками ../../) или абсолютный
-	console.log(resolve('../')); // ?!?!?! Преобразует относительный путь в абсолютный
-	console.log(sep); //Разделитель путей. В разных ОС он разный, поэтому если нужно разделить по путь используем переменную "sep"
+const saveKeyValue = async (key, value) => {
+	let data = {};
+	if(await isExist(filePath)){
+		const file = await promises.readFile(filePath);
+		data = JSON.parse(file);
+	}
+
+	data[key] = value;
+	await promises.writeFile(filePath, JSON.stringify(data));
 };
 
-export { saveKeyValue };
+const getKeyValue = async (key) => {
+	if(await isExist(filePath)){
+		const file = await promises.readFile(filePath);
+		const data = JSON.parse(file);
+		return data[key];
+	}
+	return undefined;
+}
+
+const isExist = async (pathCheck) => {
+	try{
+		await promises.stat(pathCheck);
+		return true;
+	} catch(e){
+		return false;
+	}
+}
+
+export { saveKeyValue, getKeyValue };
